ced5f2572590d8c0d8f44b74387cf90c00a40dda
Timothy Click <tcthepoet@yahoo.com>
2017-08-08 17:39:16 +0800

    Added  Python 3 compatability imports.

--- tests/test_fluctmatch.py
+++ tests/test_fluctmatch.py
@@ -1,3 +1,13 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
 
 from click.testing import CliRunner
 
@@ -8,5 +18,5 @@ def test_main():
     runner = CliRunner()
     result = runner.invoke(main, [])
 
-    assert result.output == '()\n'
+    assert result.output == "()\n"
     assert result.exit_code == 0
