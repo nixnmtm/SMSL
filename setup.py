9dd38182e65eebfdbdf9fbf6448acbf9cc0224bf
Timothy Click <tclick@nctu.edu.tw>
2017-10-16 09:25:48 +0800

    Updated the version number.
    Signed-off-by: Timothy Click <tcthepoet@yahoo.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+#
+# fluctmatch --- https://github.com/tclick/python-fluctmatch
+# Copyright (c) 2013-2017 The fluctmatch Development Team and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# Released under the New BSD license.
+#
+# Please cite your use of fluctmatch in published work:
+#
+# Timothy H. Click, Nixon Raj, and Jhih-Wei Chu.
+# Calculation of Enzyme Fluctuograms from All-Atom Molecular Dynamics
+# Simulation. Meth Enzymology. 578 (2016), 327-342,
+# doi:10.1016/bs.mie.2016.05.024.
+#
 from __future__ import (
     absolute_import,
     division,
@@ -31,7 +45,7 @@ def read(*names, **kwargs):
 
 setup(
     name="fluctmatch",
-    version="3.0.0",
+    version="3.0.2",
     license="BSD license",
     description="Elastic network model using fluctuation matching.",
     long_description="%s\n%s" % (
