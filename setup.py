c6dd7278f20a931e3516d1b9b40ae7b52f8c1cba
tclick <tcthepoet@yahoo.com>
2017-11-11 22:13:33 +0800

    Bump version: 3.2.2 → 3.2.3

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def read(*names, **kwargs):
 
 setup(
     name="fluctmatch",
-    version="3.2.2",
+    version="3.2.3",
     license="BSD license",
     description="Elastic network model using fluctuation matching.",
     long_description="%s\n%s" % (
