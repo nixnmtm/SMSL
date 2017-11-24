3467656444dc6adb75331ebfe4c787a8a90baaf2
Timothy Click <tclick@nctu.edu.tw>
2017-11-24 12:19:47 +0800

    Removed multiprocessing to fix bug
    Signed-off-by: Timothy Click <tcthepoet@yahoo.com>

--- src/fluctmatch/models/core.py
+++ src/fluctmatch/models/core.py
@@ -22,10 +22,8 @@ from __future__ import (
 )
 
 import itertools
-import multiprocessing as mp
 import warnings
 
-from future.builtins import zip
 from future.utils import (
     viewkeys,
     raise_with_traceback,
@@ -35,10 +33,6 @@ from fluctmatch import _MODELS
 from fluctmatch.models import *
 from fluctmatch.models.base import Merge
 
-
-def _map_model(model, *args):
-    filenames, kwargs = args
-    return _MODELS[model](*filenames, **kwargs)
 
 def modeller(*args, **kwargs):
     """Create coarse-grain model from universe selection.
@@ -59,17 +53,8 @@ def modeller(*args, **kwargs):
         universe = _MODELS["ENM"](*args, **kwargs)
         return universe
 
-    # universe = []
     try:
-        p = mp.Pool(maxtasksperchild=2)
-        model = p.imap(
-            _map_model,
-            zip(models, itertools.repeat(args), itertools.repeat(kwargs))
-        )
-        universe = [_ for _ in model]
-        p.close()
-        # for model in models:
-        #     universe.append(_MODELS[model](*args, **kwargs))
+        universe = [_MODELS[model](*args, **kwargs) for model in models]
     except KeyError:
         msg = (
             "{0} is not an available model. "
